cbc24abff31e55a6d8be330aa34a37d8f298cea7
SergeantJohnson <72981182+SergeantJohnson@users.noreply.github.com>
2021-04-05 09:01:18 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyAuU_usNawoSma8ud3DL8TvY-VHShvR1Sc",
+    authDomain: "halo-chatters.firebaseapp.com",
+    projectId: "halo-chatters",
+    storageBucket: "halo-chatters.appspot.com",
+    messagingSenderId: "177406424705",
+    appId: "1:177406424705:web:806dc5b76239f983383f80"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+ 
